c4e04df1c9f8396fc9ba38fafc6d58dcf1114750
ArjunChauhan0910 <arjunchau@gmail.com>
2023-04-20 22:48:01 -0400

    incl. mask for training hole-y depth masks

--- src/train.py
+++ src/train.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class Trainer:
-    def __init__(self, model, train_dataloader, test_dataloader, learning_rate = 0.001, batch_size = 100, 
+    def __init__(self, model, train_dataloader, test_dataloader, learning_rate = 0.0006, batch_size = 100, 
                     num_epochs = 20, scheduler = None, device = 'cuda'):
       
         self.model = model
@@ -26,7 +26,8 @@ class Trainer:
     def loss(self, model_out, depth_imgs_gt):
         loss_fn = torch.nn.MSELoss()
         # loss_fn = torch.nn.CrossEntropyLoss()
-        loss_val = loss_fn(model_out, depth_imgs_gt)
+        mask = (depth_imgs_gt != 0)
+        loss_val = loss_fn(model_out[mask], depth_imgs_gt[mask])
         return loss_val
     
     def train(self):
@@ -70,7 +71,7 @@ class Trainer:
             print(f"[VAL LOSS] {val_loss}")
 
         images, depth = iter(self.test_dataloader).next()
-        self.show_image(torchvision.utils.make_grid(depth[1:10],10,1))
+        self.show_image(torchvision.utils.make_grid(images[1:10],10,1), torchvision.utils.make_grid(depth[1:10],10,1))
         # plt.show()
         # plt.figure()
 
@@ -84,11 +85,16 @@ class Trainer:
         x = x.clamp(0, 1)
         return x
 
-    def show_image(self, img):
-        img = self.to_img(img)
+    def show_image(self, img, depth = None):
+        # img = self.to_img(img)
         npimg = img.numpy()
-        print(f"[INFo] Saving gt plot")
-        plt.imsave("gt_depth.png", np.transpose(npimg, (1, 2, 0)))
+        print(f"[INFO] Saving gt img plot")
+        plt.imsave("img.png", np.transpose(npimg, (1, 2, 0)))
+
+        npdepth = depth.numpy()
+        print(f"[INFO] Saving gt plot")
+        plt.imsave("gt_depth.png", np.transpose(npdepth, (1, 2, 0)))
+
 
     def visualise_output(self, images, model):
 
@@ -97,9 +103,9 @@ class Trainer:
             images = images.to(self.device)
             images = model(images)
             images = images.cpu()
-            images = self.to_img(images)
+            # images = self.to_img(images)
             np_imagegrid = torchvision.utils.make_grid(images[1:10], 10, 1).numpy()
-            print(f"[INFo] Saving pred plot")
+            print(f"[INFO] Saving pred plot")
             plt.imsave("pred_depth.png", np.transpose(np_imagegrid, (1, 2, 0)))
             # plt.show()
 
@@ -107,7 +113,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # dataset directory
     parser.add_argument('--data-dir', type=str, help='path to dataset directory', default="/home/arjun/Desktop/spring23/vlr/project/DPE/data/rgbd_dataset_freiburg2_pioneer_360")
-    parser.add_argument('--batch-size', type=str, help='batch size', default=32)
+    parser.add_argument('--batch-size', type=str, help='batch size', default=20)
 
     args = parser.parse_args()
     dataset_dir = args.data_dir
